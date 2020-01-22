a517f532218288748b294cfa8969ea041e632e7c
Fabian Acosta <acosahf@hotmail.com>
2020-01-21 23:42:29 -0600

    refactor for adminpage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import MainList from "../../components/MainList";
-import EditMainPage from "../EditMainPage/EditMainPage";
 import SignupPage from "../SignupPage/SignupPage";
 import LoginPage from "../LoginPage/LoginPage";
 import userService from "../../utils/userService";
 import * as mainAPI from "../../services/mains-api";
-import * as menuAPI from "../../services/menus-api";
-import AddMainPage from "../AddMainPage/AddMain";
+import * as wineAPI from "../../services/wines-api";
 import NavBar from "../../components/NavBar/NavBar";
+import About from "../../components/About";
+import Menu from "../../components/Menu";
+import AdminPage from "../AdminPage";
 
 class App extends Component {
   constructor() {
@@ -17,48 +17,17 @@ class App extends Component {
     this.state = {
       user: userService.getUser(),
       mains: [],
-      menus: []
+      wines: []
     };
   }
-
-  handleAddMain = async newMainData => {
-    const newMain = await mainAPI.create(newMainData);
-    this.setState(
-      state => ({
-        mains: [...state.mains, newMain]
-      }),
-      // Using cb to wait for state to update before rerouting
-      () => this.props.history.push("/")
-    );
-  };
-
-  handleUpdateMain = async updatedMainData => {
-    const updatedMain = await mainAPI.update(updatedMainData);
-    const newMainsArray = this.state.mains.map(p =>
-      p._id === updatedMain._id ? updatedMain : p
-    );
-    this.setState({ mains: newMainsArray }, () => this.props.history.push("/"));
-  };
-
-  handleDeleteMain = async id => {
-    await mainAPI.deleteOne(id);
-    this.setState(
-      state => ({
-        // Yay, filter returns a NEW array
-        mains: state.mains.filter(p => p._id !== id)
-      }),
-      () => this.props.history.push("/")
-    );
-  };
 
   async componentDidMount() {
     const mains = await mainAPI.getAll();
     this.setState({ mains });
-    const menus = await menuAPI.getAll();
-    this.setState({ menus });
+    const wines = await wineAPI.getAll();
+    this.setState({ wines });
   }
-  // async componentDidMount() {
-  // }
+
   handlelogout = () => {
     userService.logout();
     this.setState({ user: null });
@@ -77,28 +46,13 @@ class App extends Component {
             exact
             path="/"
             render={() => (
-              <MainList
-                mains={this.state.mains}
-                menus={this.state.menus}
-                handleDeleteMain={this.handleDeleteMain}
-              />
+              <div>
+                <About />
+                <Menu mains={this.state.mains} wines={this.state.wines} />
+              </div>
             )}
           />
-
-          <Route
-            path="/create"
-            render={() => <AddMainPage handleAddMain={this.handleAddMain} />}
-          />
-          <Route
-            exact
-            path="/edit"
-            render={({ location }) => (
-              <EditMainPage
-                handleUpdateMain={this.handleUpdateMain}
-                location={location}
-              />
-            )}
-          />
+          <Route path="/AdminPage" render={props => <AdminPage {...props} />} />
           <Route
             exact
             path="/signup"
