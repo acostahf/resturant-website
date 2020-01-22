423c95e41e14db5cdd4fa4de70e1e83f441d779c
Fabian Acosta <acosahf@hotmail.com>
2020-01-21 23:36:11 -0600

    create about comp

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          This is my restaurant website where you can look at the menu for main
+          dishes and the wine list.{" "}
+        </p>
+      </div>
+    );
+  }
+}
