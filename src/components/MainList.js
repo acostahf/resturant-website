345e130e1348adfc8bc43a5cb6b2326af248c590
Fabian Acosta <acosahf@hotmail.com>
2020-01-23 12:07:32 -0600

    bootstrap divs

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -5,17 +5,22 @@ import WineListItem from "../components/WineListItem/WineListItem";
 function MainListPage(props) {
   return (
     <>
-      <h1>Main List</h1>
-      <div className="MainListPage-grid">
-        {props.mains.map(main => (
-          <MainListItem
-            main={main}
-            handleDeleteMain={props.handleDeleteMain}
-            key={main._id}
-          />
-        ))}
+      <h1>MAIN LIST</h1>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="">
+            {props.mains.map(main => (
+              <MainListItem
+                main={main}
+                handleDeleteMain={props.handleDeleteMain}
+                key={main._id}
+              />
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>wine List</h1>
+
+      <h1>WINE LIST</h1>
       <div className="MenuListPage-grid">
         {props.wines.map(wine => (
           <WineListItem
