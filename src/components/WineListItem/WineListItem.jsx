335132a5f9a064eb046252aac3f3a8f67e6735e7
Fabian Acosta <acosahf@hotmail.com>
2020-01-23 12:10:04 -0600

    bootstrap update

--- src/components/WineListItem/WineListItem.jsx
+++ src/components/WineListItem/WineListItem.jsx
@@ -3,34 +3,28 @@ import { Link } from "react-router-dom";
 
 function WineListItem({ wine, handleDeleteWine }) {
   return (
-    <div>
-      <div className="">
-        <h3>{wine.name}</h3>
-        <h4>{wine.price}</h4>
-        <h4>{wine.grape}</h4>
-        <h4>{wine.region}</h4>
-        <p>{wine.description}</p>
-      </div>
-      <div>
-        {/* <Link
-            className=""
-            to={{
-              pathname: "/details",
-              state: { main }
-            }}
-          >
-            DETAILS
-          </Link> */}
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">{wine.name}</h5>
+
+        <p className="card-text">
+          This is a {wine.grape} from {wine.region}. Cost per glass is $
+          {wine.price}
+        </p>
+        <p className="card-text">Description: {wine.description}</p>
         <Link
-          className=""
           to={{
             pathname: "/AdminPage/wine/edit",
             state: { wine }
           }}
+          class="btn btn-primary"
         >
           EDIT
         </Link>
-        <button className="" onClick={() => handleDeleteWine(wine._id)}>
+        <button
+          className="btn btn-danger"
+          onClick={() => handleDeleteWine(wine._id)}
+        >
           DELETE
         </button>
       </div>
