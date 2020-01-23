d9f7234cf5f506e31e5645326ecfd67b602c49f2
Fabian Acosta <acosahf@hotmail.com>
2020-01-23 12:07:47 -0600

    bootstrap update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,18 +2,30 @@ import React from "react";
 
 export default function Menu(props) {
   return (
-    <div>
-      <h2>Main dishes </h2>
-      <div>
-        {props.mains.map(main => (
-          <h4 key={main._id}>{main.name}</h4>
-        ))}
+    <div className="row row-cols-1 row-cols-md-2">
+      <div className="card">
+        <h5 className="card-header">MAIN DISHES</h5>
+        <div className="card-body">
+          {props.mains.map(main => (
+            <h5 className="card-title" key={main._id}>
+              {main.name}
+            </h5>
+          ))}
+          {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a> */}
+        </div>
       </div>
-      <h2>Wine List</h2>
-      <div>
-        {props.wines.map(wine => (
-          <h4 key={wine._id}>{wine.name}</h4>
-        ))}
+      <div className="card">
+        <h5 className="card-header">WINE LIST</h5>
+        <div className="card-body">
+          {props.wines.map(wine => (
+            <h5 className="card-title" key={wine._id}>
+              {wine.name}
+            </h5>
+          ))}
+          {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a> */}
+        </div>
       </div>
     </div>
   );
