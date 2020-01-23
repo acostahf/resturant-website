08a9fe5757bec44c3d5fabdc6e35952cdc87ccbe
Fabian Acosta <acosahf@hotmail.com>
2020-01-23 12:06:15 -0600

    add some text to about and img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,34 @@ import React, { Component } from "react";
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>
-          This is my restaurant website where you can look at the menu for main
-          dishes and the wine list.{" "}
-        </p>
+      <div className="about">
+        <div className="about card bg-dark text-white">
+          <img
+            src={
+              "https://images.pexels.com/photos/776538/pexels-photo-776538.jpeg?cs=srgb&dl=restaurant-interior-776538.jpg&fm=jpg"
+            }
+            className="card-img"
+          />
+          <div className="card-img-overlay">
+            <h5 className="card-title">ABOUT US</h5>
+            <p className="card-text">
+              Chocolate cookie liquorice ice cream cheesecake lollipop caramels
+              macaroon icing. Gummies bear claw halvah cotton candy croissant.
+              Chocolate bar muffin tart pudding cake tart. Dessert toffee
+              cheesecake danish tiramisu jujubes macaroon macaroon. Liquorice
+              cake cheesecake pudding marzipan gingerbread. Jelly-o bear claw
+              fruitcake tart. Gummies icing dessert cheesecake macaroon bear
+              claw pie croissant. Gingerbread cake cheesecake pudding tart
+              chocolate bar caramels apple pie. Jelly tiramisu marzipan. Pastry
+              dragée soufflé wafer jelly beans macaroon pastry tart. Dessert
+              icing sugar plum cake liquorice cupcake jelly beans chocolate bar
+              soufflé. Chocolate candy canes muffin fruitcake chupa chups
+              chocolate bar. Tiramisu marshmallow sweet roll. Cookie bonbon
+              powder pastry chocolate.
+            </p>
+            <p className="card-text"></p>
+          </div>
+        </div>
       </div>
     );
   }
