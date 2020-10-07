a603ac1aaa1198e49d63b0cb72e7cd2407288e80
acostahf <acostahf4@gmail.com>
2020-10-06 22:59:16 -0500

    changed base url to http://localhost:4000/api/mains since i was getting a 431 error

--- src/services/mains-api.js
+++ src/services/mains-api.js
@@ -1,27 +1,27 @@
-const BASE_URL = "/api/mains";
+const BASE_URL = "http://localhost:4000/api/mains";
 
 export function getAll() {
-  return fetch(BASE_URL).then(res => res.json());
+  return fetch(BASE_URL).then((res) => res.json());
 }
 
 export function create(main) {
   return fetch(BASE_URL, {
     method: "POST",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(main)
-  }).then(res => res.json());
+    body: JSON.stringify(main),
+  }).then((res) => res.json());
 }
 
 export function update(main) {
   return fetch(`${BASE_URL}/${main._id}`, {
     method: "PUT",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(main)
-  }).then(res => res.json());
+    body: JSON.stringify(main),
+  }).then((res) => res.json());
 }
 
 export function deleteOne(id) {
   return fetch(`${BASE_URL}/${id}`, {
-    method: "DELETE"
-  }).then(res => res.json());
+    method: "DELETE",
+  }).then((res) => res.json());
 }
